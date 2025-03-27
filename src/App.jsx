d12d777f46175c4d3d42bf6d9e770359c274838a
rwam1 <rwam1.nikhil.ub@gmail.com>
2025-03-27 12:17:15 +0530

    add file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div>
-      
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. At facilis
+      aliquid a, provident eaque accusamus eos, architecto libero consequatur
+      ipsum veritatis exercitationem quo.
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
